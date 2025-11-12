5727ac4438c26972ff27313ea2a3b612c6b9191e
countynetkenya <122329385+countynetkenya@users.noreply.github.com>
2025-11-12 15:28:51 +0300

    Gracefully handle missing Kint dependency

--- app/Config/Boot/development.php
+++ app/Config/Boot/development.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once __DIR__ . '/KintGuard.php';
+
+$kintAvailable = kint_guard_detect_init_file() !== null;
+
 /*
  |--------------------------------------------------------------------------
  | ERROR DISPLAY
@@ -31,4 +35,8 @@ defined('SHOW_DEBUG_BACKTRACE') || define('SHOW_DEBUG_BACKTRACE', true);
  | the system. This will control whether Kint is loaded, and a few other
  | items. It can always be used within your own application too.
  */
-defined('CI_DEBUG') || define('CI_DEBUG', true);
+defined('CI_DEBUG') || define('CI_DEBUG', $kintAvailable);
+
+if (! $kintAvailable) {
+    kint_guard_register_fallback();
+}
